060553eb900d5c79df43b928660843e257dd1c5a
NGUYỄN HUY <huy.nthanh79@gmail.com>
2022-06-02 10:49:50 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-            const route = require('./routes');
-        const db = require('./config/db');
+    const route = require('./routes');
+    const db = require('./config/db');
 
 //Connect to DB
-    db.connect();
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
